0c7e049d783368f1ec828fd8bc930fe5e35ca9f5
Josh-Lv <747923860@qq.com>
2016-10-19 18:14:23 +0800

    完成云医通首页布局

--- testDemo/pages/Cloudhospital/Cloudhospital.js
+++ testDemo/pages/Cloudhospital/Cloudhospital.js
@@ -1,9 +1,13 @@
+// var homePage = getHome();
+
 Page({
   data:{
     // text:"这是一个页面"
   },
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
+    console.log(options.hospitalList)
+    let hospitalList = hospitalList
   },
   onReady:function(){
     // 页面渲染完成
